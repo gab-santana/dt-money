7b549c08aefbb26c5ee8d263875214b0ecee7e57
Gabriel Santana <gabrielsantana100@hotmail.com>
2023-06-17 21:02:36 -0300

    applying react-hook-form

--- src/components/NewTransactionModal/styles.ts
+++ src/components/NewTransactionModal/styles.ts
@@ -50,8 +50,13 @@ form{
     border-radius: 6px;
     margin-top: 1.5rem;
     cursor: pointer;
+    
+    &:disabled{
+      opacity: 0.6;
+      cursor: not-allowed
+    }
 
-    &:hover{
+    &:not(:disabled):hover{
       background: ${props => props.theme['green-700']};
       transition: background-color 0.2s;
     }
