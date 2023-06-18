0494b0fcc6fa9aab26f48f1e0991ed1e9448198f
Gabriel Santana <gabrielsantana100@hotmail.com>
2023-06-18 01:05:51 -0300

    create new transaction

--- src/contexts/TransactionsContext.tsx
+++ src/contexts/TransactionsContext.tsx
@@ -10,9 +10,18 @@ interface ITransaction {
   created_at: string;
 }
 
+interface ICreateTransactionInput {
+  description: string;
+  type: 'income' | 'outcome';
+  price: number;
+  category: string;
+
+}
+
 interface ITransactionContextType {
   transactions: ITransaction[]
   fetchTransactions: (query?: string) => Promise<void>
+  createTransaction: (data: ICreateTransactionInput) => Promise<void>
 }
 
 interface ITransactionsProviderProps {
@@ -28,18 +37,36 @@ export function TransactionsProvider({ children }: ITransactionsProviderProps) {
   async function fetchTransactions(query?: string) {
     const response = await api.get('/transactions', {
       params: {
+        _sort: 'created_at',
+        _order: 'desc',
         q: query
       }
     })
-
     setTransactions(response.data)
   }
+
+  async function createTransaction(data: ICreateTransactionInput) {
+    const { category, description, price, type } = data
+
+    const response = await api.post('transactions', {
+      description,
+      category,
+      price,
+      type,
+      created_at: new Date(),
+
+
+    })
+
+    setTransactions(state => [response.data, ...state])
+  }
+
   useEffect(() => {
 
     fetchTransactions()
   }, [])
   return (
-    <TransactionsContext.Provider value={{ transactions, fetchTransactions }
+    <TransactionsContext.Provider value={{ transactions, fetchTransactions, createTransaction }
     }>
       {children}
     </TransactionsContext.Provider >
