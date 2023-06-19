ba33e848b3729b7eda88217eed4c81ccc13de497
Gabriel Santana <gabrielsantana100@hotmail.com>
2023-06-19 14:14:41 -0300

    applying context selector for rendering performance

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -5,7 +5,7 @@ import * as z from 'zod'
 import { Controller, useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { TransactionsContext } from "../../contexts/TransactionsContext";
-import { useContext } from 'react'
+import { useContextSelector } from 'use-context-selector'
 
 const newTransactionFormSchema = z.object({
   description: z.string(),
@@ -18,7 +18,9 @@ type NewTransactionFormInputs = z.infer<typeof newTransactionFormSchema>
 
 export function NewTransactionModal() {
 
-  const { createTransaction } = useContext(TransactionsContext)
+  const createTransaction = useContextSelector(TransactionsContext, (context) => {
+    return context.createTransaction
+  })
   const { control, register, handleSubmit, formState: {
     isSubmitting
   }, reset } = useForm<NewTransactionFormInputs>({
